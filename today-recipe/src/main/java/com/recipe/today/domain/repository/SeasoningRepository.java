3d4e8d7012738b34b9d5ec0b8780c6af36aca42c
'mapirin' <'812matahiro29@gmail.com'>
2024-10-27 22:22:21 +0900

    20241027
    レシピ登録時の食材と調味料データ格納処理の追加
    Repositoryに食材と調味料の登録処理を追加

--- today-recipe/src/main/java/com/recipe/today/domain/repository/SeasoningRepository.java
+++ today-recipe/src/main/java/com/recipe/today/domain/repository/SeasoningRepository.java
@@ -1,10 +1,10 @@
 package com.recipe.today.domain.repository;
 
-import com.recipe.today.domain.entity.SeasoningMasterDTO;
+import com.recipe.today.domain.entity.ListDTO;
 
 public interface SeasoningRepository {
 	
 	public int duplicateCheck(String recipeName);
 	
-	public void i(SeasoningMasterDTO seasoningMasterDTO);
+	public void i(ListDTO listDTO);
 }
